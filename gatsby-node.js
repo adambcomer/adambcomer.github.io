20ca7385bc3ccd476126720d38f562a3ce803158
Adam Comer <adambcomer@gmail.com>
2021-02-03 15:34:41 -0500

    Netlify redirect test

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,36 +33,4 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       },
     })
   })
-
-
-  createRedirect({
-    fromPath: '/blog/simple-database.html',
-    isPermanent: true,
-    redirectInBrowser: true,
-    toPath: '/blog/simple-database'
-  })
-  createRedirect({
-    fromPath: '/blog/simple-database/motivation-design.html',
-    isPermanent: true,
-    redirectInBrowser: true,
-    toPath: '/blog/simple-database/motivation-design'
-  })
-  createRedirect({
-    fromPath: '/blog/simple-database/memtable.html',
-    isPermanent: true,
-    redirectInBrowser: true,
-    toPath: '/blog/simple-database/memtable'
-  })
-  createRedirect({
-    fromPath: '/blog/simple-database/wal.html',
-    isPermanent: true,
-    redirectInBrowser: true,
-    toPath: '/blog/simple-database/wal'
-  })
-  createRedirect({
-    fromPath: '/blog/setup-gitlab-cicd-on-kubernetes.html',
-    isPermanent: true,
-    redirectInBrowser: true,
-    toPath: '/blog/install-gitlab-runner-kubernetes'
-  })
 }
