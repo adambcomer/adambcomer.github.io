1c1af892f7f3bfcb353eeffbc8999045f53c98a9
Adam Comer <adambcomer@gmail.com>
2021-04-09 14:49:37 -0400

    Added better font loading

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
 import './src/styles/global.css'
+
+import '@fontsource/ibm-plex-sans/300.css'
+import '@fontsource/ibm-plex-sans/400.css'
+import '@fontsource/ibm-plex-sans/400-italic.css'
+import '@fontsource/ibm-plex-sans/600.css'
+
+import '@fontsource/ibm-plex-mono/400.css'
+import '@fontsource/ibm-plex-mono/400-italic.css'
+import '@fontsource/ibm-plex-mono/600.css'
